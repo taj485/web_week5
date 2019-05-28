e74c1010fdd603e05bbfd57552f84aabc8606414
edwardreeve <edreeve+git@gmail.com>
2019-05-28 16:27:37 +0100

    few more routes - shotgun
    ç
    exit

--- app.rb
+++ app.rb
@@ -3,3 +3,19 @@ require 'sinatra'
 get '/' do
   "Hello World"
 end
+
+get '/secret' do
+  "What don't you understand about 'secret'?"
+end
+
+get '/edspage' do
+  "WOOOHOOOOO! GREAT PAGE!"
+end
+
+get '/tajspage' do
+  "I CAN'T QUITE BELIEVE HOW AMAZING THIS PAGE IS"
+end
+
+get '/about-us' do
+  "Programmers par excellence"
+end
