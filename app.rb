2221015315c0c71c1a71a25d7c44784fdba4a124
Taj <tajbi21@gmail.com>
2019-05-29 15:44:38 +0100

    useing forms completed

--- app.rb
+++ app.rb
@@ -13,9 +13,33 @@ get '/edspage' do
 end
 
 get '/tajspage' do
+  erb "Hi there, Visitor!"
   "I CAN'T QUITE BELIEVE HOW AMAZING THIS PAGE IS"
 end
 
 get '/about-us' do
-  "Programmers par excellence"
+  "<h1>Hello World</h1>"
+end
+
+get '/random-cat' do
+  @name = ["tom", "charlie","jake"].sample
+  erb(:index)
+end
+
+get '/named-cat'do
+  p params
+  @name = params[:name]
+  erb(:index)
+end
+
+get '/whatsup' do
+  "Whats up " + params[:name] + " " + params[:surname]
+end
+
+get '/pathurl/:name/:surname' do
+  "Whats up " + params[:name] + " " + params[:surname]
+end
+
+get '/splat/*/to/*' do
+  params[:splat][0] + ' ' + params[:splat][1] + '!'
 end
